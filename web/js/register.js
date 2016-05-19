d1e8875db9bafbdeae3b555bbcefcd6e47415fac
zhiminmu <m18813759751@163.com>
2016-05-19 20:26:54 +0800

    2015-5-19

---
+++ web/js/register.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+	$(".username").focus(function() {
+		$(this).addClass("focus");
+		$(".username-group label").css({
+			display: 'none'
+		});
+		var userVal=$(this).val();
+		console.log(userVal);
+		if (userVal=="username") {
+			$(this).val("");
+		}
+	}).blur(function(){
+		$(this).removeClass("focus");
+		checkUsername($(this).val());
+		
+	});
+
+	$(".password").focus(function() {
+		$(this).addClass("focus");
+		$(".password-group label").css({
+			display: 'none'
+		});
+		var userVal=$(this).val();
+		if (userVal=="password") {
+			$(this).val("");
+		}
+	}).blur(function(){
+		$(this).removeClass("focus");
+		checkPassword($(this).val());
+		
+	});
+
+	$(".confpsw").focus(function() {
+		$(this).addClass("focus");
+		$(".confpsw-group label").css({
+			display: 'none'
+		});
+		var userVal=$(this).val();
+		if (userVal=="confirm password") {
+			$(this).val("");
+		}
+	}).blur(function(){
+		$(this).removeClass("focus");
+		checkConfpsw($(".password").val(),$(this).val());
+		
+	});
+
+
+function checkUsername(username){
+	var reg = /[a-z_]{1}\w{5,15}/;
+	var result = username.match(reg);
+	if (result ==null) {
+		$(".username-group label").css({
+			display: 'inline'
+		});
+	}
+}
+function checkPassword(password){
+	var len = password.length;
+	if (len<=8 || len>=16) {
+		$(".password-group label").css({
+			display: 'inline'
+		});
+	}
+}
+
+function checkConfpsw(password,confpsw){
+
+	if (password!=confpsw) {
+		$(".confpsw-group label").css({
+			display: 'inline'
+		});
+	}
+}
+
+$(".register").click(function(event) {
+
+	$.post('/path/to/file', {param1: 'value1'}, function(data, textStatus, xhr) {
+		
+	});
+});
+
+});
