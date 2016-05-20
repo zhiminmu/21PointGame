559386dc725a7308b717522fb97f7c95c7538c4b
txh6634125 <txh6634125@sina.com>
2016-05-20 16:31:49 +0800

    login-16:31

--- app_dev.php
+++ app_dev.php
@@ -1,2 +1,13 @@
-<?php  
-	echo "hello php!";
+<?php
+	require_once __DIR__."/Service/AppKernel.php";
+
+	use Service\AppKernel;
+
+	$kernel = AppKernel::getAppKernel();
+
+	$request_url = $_SERVER["REQUEST_URI"];
+
+	$kernel->controler($request_url);
+	//echo $kernel->getCount();
+	
+
